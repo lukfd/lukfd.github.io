47c966c91d15a1d5ef0264478d6deae7fede8e06
lukfd <comb6457@stthomas.edu@users.noreply.github.com>
2020-11-10 18:51:10 -0600

    changing

--- map/script.js
+++ map/script.js
@@ -1,25 +1,39 @@
-// Luca Comba
-// Organization list
+/*Author: Luca Comba (luca.comba98@gmail.com)
+  For University of St. Thomas' STELAR
+*/ 
+
+/* Organization list that will appear in the map
+
+  Check README.md for more details to create a new popup
+*/
 var orgs = [
   {   "name": "Archdiocese Commission for Ecumenical and Interfaith Relations",
       "address": "2028, Grand Avenue",
       "city": "St. Paul",
-      "contact person":"",
-      "phone":"",
-      "email":"",
+      "contact_person":"",
+      "phone":"612 707 9749",
+      "email":"email@email.com",
       "category":"sfc",
+      "tag":"prova, trova",
+      "description":"description",
       "website":"",
-      "logo":""
+      "img":"https://www.arcgis.com/sharing/rest/content/items/a21e3de5051f4549b1a9cff9c23badf7/resources/apeaceofmind__1591118209890__w1327.jpg",
+      "latlng":[],
+      "marker":null
   },
   {   "name": "Try",
       "address": "2245, Summit Avenue",
       "city": "St. Paul",
-      "contact person":"",
-      "phone":"",
-      "email":"",
+      "contact_person":"",
+      "phone":"612 707 9749",
+      "email":"email@email.com",
       "category":"sfo",
+      "tag":"loon",
+      "description":"description",
       "website":"",
-      "logo":""
+      "img":"https://www.arcgis.com/sharing/rest/content/items/a21e3de5051f4549b1a9cff9c23badf7/resources/apeaceofmind__1591118209890__thumb.jpg",
+      "latlng":[],
+      "marker":null
   }
 ];
 
@@ -67,8 +81,6 @@ function Init() {
   if (app.data.length == 0) {
     app.data = orgs;
   }
-  
-  //var temp_list = [];
 
   app.data.forEach((obj) => {
     /*
@@ -92,48 +104,72 @@ function Init() {
     };
     // make get request
     $.ajax(request).then(() => {
-      var marker = L.marker([addressLat, addressLng]).addTo(mymap);
-      
-      marker.bindPopup(
-        "<h3>"+obj.name+"</h3>");
+      // Create POP UP
+      var marker = L.marker([addressLat, addressLng], title = obj.name).addTo(mymap);
 
-      //temp_list.push(marker);
+      // adding to obj.latlng FOR OPENING THE POP UP
+      obj.latlng = [addressLat, addressLng];
+
+      // Create POP UP
+      marker.bindPopup(
+        "<h3>"+obj.name+"</h3>"+
+        '<img src="'+ obj.img +'" height="80px">'+
+        '<div style="padding-top:3px;"></div>'+
+        '<p>'+obj.description+'</p>'+
+        '<div style="padding-top:3px;"></div>'+
+        '<a href="'+obj.website+'">'+'visit their website'+'</a><br>'+
+        '<a href="mailto:'+obj.email+'">'+'contact them at:'+obj.email+'</a>'+
+        '<p>'+obj.contact_person+' '+obj.phone+'</p><br>'+
+        '<p>Address: '+obj.address+'</p>', {
+          minWidth : 100,
+          autoClose: false
+      });
+
+      // add to Vue app's list markers that contains all markers
+      app.markers.push(marker)
+
+      // add to obj.marker the marker object
+      obj.marker = marker;
     });
     
   });
-  // var markers = L.layerGroup(temp_list);
-
-  // var overlay = {
-  //   "Markers": markers
-  // }
-  // //console.log(temp_list);
-  // L.control.layers(overlay).addTo(mymap);
-  // mymap.addLayer(overlay); 
   
 }
 
+/* Filters the three categories of #checkbox in index.html
+   if thery are all three options selected then close all popups
+   othewise open popups of marker that have the searchTypeOption
+*/
 function Filter(event) {
-  app.data = orgs;
-  var newData = [];
-  app.data.forEach((obj) => {
-    for (var i = 0; i < app.searchTypeOptions.length; i++) {
-      if (obj.category == app.searchTypeOptions[i]) {
-        newData.push(obj);
+  // open popup only if one or two searchTypeOptions are selected
+  if (app.searchTypeOptions.length < 3) {
+    // which one to show
+    app.data.forEach((obj) => {
+      for (var i = 0; i < app.searchTypeOptions.length; i++) {
+        if (obj.category == app.searchTypeOptions[i]) {
+          obj.marker.openPopup();
+        }
       }
-    }
-  });
-
-  // updating app's data
-  app.data = newData;
-  // TO-DO
-  // Clear map markers
-  // remove all the markers in one go
-  app.markers.clearLayers();
-  // Update Markers
-
+    });
+  } else {
+    // close all popups
+    app.markers.forEach((marker) => {
+      marker.closePopup();
+    });
+  }
+  
 }
 
+/* Search a text in the names, 
+  if not present just look for an address
+
+  BUG: lat lng to undefined location
+*/
 function Search(event) {
+  // TO-DO
+  // if user input a tag
+  // Open POP UP when research found name or tag
+
   /*
   QUERY : https://nominatim.openstreetmap.org/search?<params>
   	street=<housenumber> <streetname>
@@ -147,11 +183,20 @@ function Search(event) {
   var streetName = app.input.replace(/\s/g, '%20');
   // Check if they typed a name
   app.data.forEach((obj) => {
+    // typed the name
     if (obj.name.includes(app.input)) {
-      streetName = obj.address.replace(/\s/g, '%20');;
+      streetName = obj.address.replace(/\s/g, '%20');
+
+      // fly to the latlng
+      //mymap.flyTo(obj.latlng, 15);
+      obj.marker.openPopup();
+    }
+    // typed the a tag
+    if (obj.tag.includes(app.input)) {
+      obj.marker.openPopup();
     }
   });
-  // MAKE THE SEARCH
+  // OTHERWISE MAKE STREET SEARCH
   let request = {
         url: queryNominatim + 'street='+ streetName +'&state=MN&format=json',
         dataType: "json",
@@ -164,6 +209,8 @@ function Search(event) {
   });	
 }
 
+/*  Function for the Nominatim.com get request
+*/
 function setNominatim(data){
   addressLat = data[0].lat;
   addressLng = data[0].lon;
