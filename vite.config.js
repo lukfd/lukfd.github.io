d9990c99e796f3c44240848dbf8d6bc5fe3bb8c9
lukfd <luca.comba98@gmail.com>
2025-05-06 23:09:43 -0500

    First round of changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // FILE: vite.config.js
 
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
@@ -8,13 +9,15 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls },
+      template: { transformAssetUrls }
     }),
 
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
-      sassVariables: 'src/quasar-variables.sass',
-    }),
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      )
+    })
   ]
 })
